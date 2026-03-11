2da9801f4d19ddeda1b9697a5aa2e7283298f159
Nathi <babiiyaga@gmail.com>
2026-03-11 13:54:35 +0200

    Second Commit of the Movies Database Prohject 2 (CREATE) module
    Updated CreatingData.java class
    Updated DataAdmin.java

--- src/CreatingData.java
+++ src/CreatingData.java
@@ -1,10 +1,9 @@
-//
-
+// Importing necessary libraries for the CreatingData.java class
 import java.sql.Date;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-//
+// Main method
 public class CreatingData {
 
 public static void main(String[] args) throws SQLException{
@@ -15,19 +14,15 @@ public static void main(String[] args) throws SQLException{
 
     //
     System.out.println("\n*** Adding Movie Data ***");
-
     //
     System.out.print("\nEnter Movie's Title: ");
     String movieName = sc.nextLine();
-
     //
     System.out.print("Enter Movie's Genre: ");
     String genre = sc.nextLine();
-
     //
     System.out.print("Enter Movie's Release Date: ");
     Date ReleaseDate = Date.valueOf(sc.nextLine());
-
     //
     DataAdmin adm = new DataAdmin();
     adm.DataUpload(movieName, genre, ReleaseDate);
--- src/DataAdmin.java
+++ src/DataAdmin.java
@@ -1,35 +1,36 @@
-//
+// Importing necessary libraries for the DataAdmin.java class
 import java.sql.*;
 
-//
+// DataAdmin class (Administrative methods to the system's database)
 public class DataAdmin {
 
-    //
+    // DataUpload Method (uploads )
     public void DataUpload(String movieName, String genre, Date releaseDate) throws SQLException {
 
+        // Creating a new object for DBConnectionManager
         DBConnectionManager dbcm = new DBConnectionManager();
 
-        //SQL query to insert data into specified database table (movies table)
+        // SQL query to insert data into specified database table (movies table)
         String DataCreateQuery = "INSERT INTO movies (Title, Genre, Release_Date) VALUES (?, ?, ?)";
 
-        //
+        // Try-Catch block of code
         try(Connection connection = DBConnectionManager.getConnection();
             PreparedStatement pstmt = connection.prepareStatement(DataCreateQuery);){
-
-            //
+            // Parameters which insert data into database (Data provided by user in CreatingData.java class)
             pstmt.setString(1, movieName);
             pstmt.setString(2, genre);
             Date ReleaseDate = null;
             pstmt.setDate(3,ReleaseDate);
 
+            //
             int rowsAffected = pstmt.executeUpdate();
 
-            //
+            // Should movie data be added into the database, a message will appear
             if(rowsAffected>0){
                 System.out.println("Data has been uploaded successfully");
             }
         }
-        //
+        // Catch block to return SQL-related exception message(s)
         catch(SQLException e){
             e.printStackTrace();
         }
